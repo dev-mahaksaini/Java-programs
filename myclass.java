e0d0848c732ddcafaac9c5221741286f6591238d
Mahak <mahaksaini1908@gmail.com>
2024-08-07 13:10:19 +0530

    Add files via upload

---
+++ myclass.java
@@ -0,0 +1,114 @@
+/*print hello world
+public class myclass
+{
+    public static void main(String args[])
+    {
+       
+        System.out.println("hello world");
+        
+    }
+}*/
+
+
+//operators (+,-,/,*,%)
+/*addition 
+public class myclass
+{
+    public static void main(String args[])
+    {
+        int a1=4 , b1=5 , sum;
+        sum=a1+b1;
+        System.out.println(sum);
+        
+    }
+}*/
+
+
+/*subtraction 
+public class myclass
+{
+    public static void main(String args[])
+    {
+        int a1=56 , b1=34 , sub;
+        sub=a1-b1;
+        System.out.println(sub);
+        
+    }
+}*/
+
+/*multiply
+public class myclass
+{
+    public static void main(String args[])
+    {
+        int a1=4 , b1=5 , mul;
+        mul=a1*b1;
+        System.out.println(mul);
+        
+    }
+}*/
+
+/*division
+public class myclass
+{
+    public static void main(String args[])
+    {
+        int a1=35 , b1=5 , div;
+        div=a1/b1;
+        System.out.println(div);
+        
+    }
+}*/
+
+/*modolo
+public class myclass
+{
+    public static void main(String args[])
+    {
+        int a1=34 , b1=5 , mod;
+        mod=a1%b1;
+        System.out.println(mod);
+        
+    }
+}*/
+
+/*write a program to factorial a number using resursion 
+public class myclass
+{
+    public static void main(String args[])
+    {
+         int res=1;  int n= 5;
+         for( int i=1;i<=5;i++)
+         {
+            res*=i;
+            System.out.println(res);
+         }
+    }
+}*/
+
+
+// write a program tp print first 15 numbers in fibbonaci series
+/*public class myclass
+{
+    public static void main(String args[] )
+  {
+    int n=15 , first_term=0, second_term=1 ;
+    System.out.println(n);
+    for(int i =0;i<=n;++i)
+    {
+        System.out.println(first_term);
+    }
+    int next_term = first_term+second_term;
+    first_term= second_term;
+    second_term= next_term;
+  }
+}*/
+
+
+
+
+
+
+
+
+
